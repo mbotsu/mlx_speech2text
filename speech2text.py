0f538fd1957408a513fee9c02bb79a2cff174a28
mbotsu <m.otsu@infocom.co.jp>
2024-07-10 14:25:43 +0900

    Display progress time of varbose correctly

--- speech2text.py
+++ speech2text.py
@@ -1,6 +1,8 @@
 import os
+import sys
 import argparse
 import mlx_whisper
+from mlx_whisper.transcribe import _format_timestamp
 from writers import get_writer
 import torch
 torch.set_num_threads(1)
@@ -17,7 +19,7 @@ pre_speech_pad_frames = 2
 post_speech_pad_frames = 2
 SAMPLING_RATE = 16000
 
-def main(speech_file, output_dir, language, write_file="out"):
+def main(speech_file, output_dir, language, verbose, write_file="out"):
     wav = read_audio(speech_file, sampling_rate=SAMPLING_RATE)
 
     speech_timestamps = get_speech_timestamps(wav, model,
@@ -28,6 +30,14 @@ def main(speech_file, output_dir, language, write_file="out"):
     result = {
         'segments': []
     }
+
+    system_encoding = sys.getdefaultencoding()
+    if system_encoding != "utf-8":
+        make_safe = lambda x: x.encode(system_encoding, errors="replace").decode(
+            system_encoding
+        )
+    else:
+        make_safe = lambda x: x
 
     wav_length = len(wav)
     for seek in speech_timestamps:
@@ -41,7 +51,7 @@ def main(speech_file, output_dir, language, write_file="out"):
         
         wav_slice = wav[seek_start:seek_end]
         start = round(seek['start'] / SAMPLING_RATE, 1)
-        # end = round(seek['end'] / SAMPLING_RATE, 1)
+        end = round(seek['end'] / SAMPLING_RATE, 1)
         speech_timestamps2 = get_speech_timestamps(wav_slice, model,
                                                     sampling_rate=SAMPLING_RATE,
                                                     return_seconds=False)
@@ -51,7 +61,7 @@ def main(speech_file, output_dir, language, write_file="out"):
         audio = collect_chunks(speech_timestamps2, wav_slice)
         audio = audio.to('cpu').detach().numpy().copy()
 
-        trans = mlx_whisper.transcribe(audio, verbose=True, language=language,
+        trans = mlx_whisper.transcribe(audio, verbose=False, language=language,
                                     path_or_hf_repo="mlx-community/whisper-large-v3-mlx", fp16=True)
 
         for r in trans['segments']:
@@ -60,7 +70,10 @@ def main(speech_file, output_dir, language, write_file="out"):
                 'end': start + r['end'],
                 'text': r['text'],
             }
-            result['segments'].append(res)    
+            if verbose:
+                result['segments'].append(res)
+                line = f"[{_format_timestamp(res['start'])} --> {_format_timestamp(res['end'])}] {res['text']}"
+                print(make_safe(line))        
         
     writer_args = {'highlight_words': False, 'max_line_count': None, 'max_line_width': None, 'pretty_json': False}
 
@@ -77,6 +90,7 @@ if __name__ == '__main__':
     parser.add_argument('-i', '--input', help='input audio file', required=True)
     parser.add_argument('-o', '--output', default="track", help='output text folder')
     parser.add_argument('-l', '--language', default="ja", help='language')
+    parser.add_argument('-v', '--varbose', action='store_true', help='varbose')
     args = parser.parse_args()
 
-    main(args.input, args.output, args.language, write_file="out")
+    main(args.input, args.output, args.language, args.varbose, write_file="out")
